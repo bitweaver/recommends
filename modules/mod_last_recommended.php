e4b64e12f7ae6eab693124b4ff813351b8113a70
wjames5 <will@tekimaki.com>
2007-04-02 15:49:53 +0000

    sort last recommends module list on recommendation qualification date

--- modules/mod_last_recommended.php
+++ modules/mod_last_recommended.php
@@ -11,7 +11,7 @@ require_once( USERS_PKG_PATH.'BitUser.php' );
 
 global $gBitSmarty, $gQueryUserId, $module_rows, $module_params, $gBitSystem;
 
-$listHash = array( 'user_id' => $gQueryUserId, 'sort_mode' => 'created_desc', 'max_records' => $module_rows, 'parse_data' => TRUE );
+$listHash = array( 'user_id' => $gQueryUserId, 'max_records' => $module_rows, 'parse_data' => TRUE );
 if ( isset($module_params['content_type']) ){ $listHash['content_type'] = $module_params['content_type'];}
 $listHash['timeout'] = $gBitSystem->getUTCTime() - ($gBitSystem->getConfig('recommends_timout_days', 15) * RECOMMENDS_TIMEOUT_DAYS_SCALE);
 $listHash['recommends'] = $gBitSystem->getConfig('recommends_minimum_recommends', '10');
