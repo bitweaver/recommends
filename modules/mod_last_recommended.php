e809203fb16bd3ce861f5868f45439788e5fbf9b
wjames5 <will@tekimaki.com>
2007-04-03 18:01:23 +0000

    rewrite module to clean up code. template is still a bit messy

--- modules/mod_last_recommended.php
+++ modules/mod_last_recommended.php
@@ -1,29 +1,18 @@
 <?php
-/**
- * required setup
- */
-if( !defined( 'MAX_RECOMMENDS_PREVIEW_LENGTH' ) ) {
-	define ('MAX_RECOMMENDS_PREVIEW_LENGTH', 100);
-}
-
 include_once( RECOMMENDS_PKG_PATH.'LibertyRecommends.php' );
-require_once( USERS_PKG_PATH.'BitUser.php' );
 
 // moduleParams contains lots of goodies: extract for easier handling
 extract( $moduleParams );
 
-$listHash = array( 'user_id' => $gQueryUserId, 'max_records' => $module_rows, 'parse_data' => TRUE );
-if ( isset($module_params['content_type']) ){ $listHash['content_type'] = $module_params['content_type'];}
-$listHash['timeout'] = $gBitSystem->getUTCTime() - ($gBitSystem->getConfig('recommends_timout_days', 15) * RECOMMENDS_TIMEOUT_DAYS_SCALE);
-$listHash['recommends'] = $gBitSystem->getConfig('recommends_minimum_recommends', '10');
-
+$listHash = array(
+	'user_id'      => $gQueryUserId,
+	'max_records'  => $moduleParams['module_rows'],
+	'parse_data'   => TRUE,
+	'content_type' => !empty( $module_params['content_type'] ) ? $module_params['content_type'] : NULL,
+	'timeout'      => $gBitSystem->getUTCTime() - ( $gBitSystem->getConfig( 'recommends_timout_days', 15 ) * RECOMMENDS_TIMEOUT_DAYS_SCALE ),
+	'recommends'   => $gBitSystem->getConfig( 'recommends_minimum_recommends', '10' ),
+);
 $recommend = new LibertyRecommends();
-$ranking = $recommend->getList( $listHash );
-
-$maxPreviewLength = (!empty($modParams['max_preview_length']) ? $modParams['max_preview_length'] : MAX_RECOMMENDS_PREVIEW_LENGTH);
-
-$gBitSmarty->assign('maxPreviewLength', $maxPreviewLength);
-$gBitSmarty->assign('modLastRecommends', $ranking);
-$gBitSmarty->assign('modLastrecommendsTitle',(isset($module_params["title"])?$module_params["title"]:""));
-$gBitSmarty->assign('recommendsPackageActive', $gBitSystem->isPackageActive('recommends'));
+$gBitSmarty->assign( 'modLastRecommends', $recommend->getList( $listHash ));
+$gBitSmarty->assign( 'maxPreviewLength', ( !empty( $modParams['max_preview_length'] ) ? $modParams['max_preview_length'] : 100 ));
 ?>
--- modules/mod_last_recommended.tpl
+++ modules/mod_last_recommended.tpl
@@ -1,14 +1,14 @@
 {strip}
-{if $recommendsPackageActive}
+{if $gBitSystem->isPackageActive( 'recommends' )}
 	{bitmodule title="$moduleTitle" name="last_recommended"}
-		<ul class="recommends">
+		<ul class="data">
 			{section name=ix loop=$modLastRecommends}
 				<li class="{cycle values="odd,even"}">
-					<div class="title">{$modLastRecommends[ix].title}</div>			
+					<div class="title">{$modLastRecommends[ix].title}</div>
 					<div class="date">{$modLastRecommends[ix].created|bit_long_date}
 					<br />
 					by {displayname hash=$modLastRecommends[ix]}</div>
-					<a href="{$modLastRecommends[ix].post_url}">Read more</a>
+					<a class="more" href="{$modLastRecommends[ix].display_url}">Read more</a>
 				</li>
 			{sectionelse}
 				<li></li>
